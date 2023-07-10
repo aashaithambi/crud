55341b08f871830aa08eca4f70d722500f2876ca
aashaithambi <asaithampirio@gmail.com>
2023-07-10 15:09:41 +0530

    django project a python file

---
+++ django/Django_Project/My_Application/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class register(models.Model):
+    Name = models.CharField(max_length=20)
+    Address = models.CharField(max_length=20)
+    Username = models.CharField(max_length=20)
+    Password = models.CharField(max_length=20)
